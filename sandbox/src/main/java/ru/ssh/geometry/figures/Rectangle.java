782488b8155f79058b508d92fbb192176991f72e
SeregaOttor <52317820+SeregaOttor@users.noreply.github.com>
2023-09-23 12:25:39 +0300

    Лекция 2.1-3

--- sandbox/src/main/java/ru/ssh/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/ssh/geometry/figures/Rectangle.java
@@ -1,6 +1,12 @@
 package ru.ssh.geometry.figures;
 
 public record Rectangle(double a, double b) {
+    public Rectangle {
+        if (a < 0||b < 0){
+            throw new IllegalArgumentException("Rectangle side should be not-negative");
+        }
+    }
+
     public static void printRectargleArea(double a, double b) {
         var text = String.format("Площадь прямоугольника со сторонами %f и %f = %f", a, b, rectangleArea(a, b));
         System.out.println(text);
--- sandbox/src/main/java/ru/ssh/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/ssh/geometry/figures/Square.java
@@ -1,7 +1,11 @@
 package ru.ssh.geometry.figures;
 
 public record Square(double side) {
-
+    public Square {
+        if (side < 0) {
+            throw new IllegalArgumentException("Square side should be not-negative");
+        }
+    }
     public static void printSquareArea(Square s) {
         String text = String.format("Площадь квадрата со стороной %f = %f", s.side, s.area());
         System.out.println(text);
