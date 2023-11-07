3715c89ac2b2face99e292d5ccd8c3ee36d764d5
SeregaOttor <52317820+SeregaOttor@users.noreply.github.com>
2023-11-07 19:44:02 +0300

    Задание 16 фикс

--- addressbook_web_tests/src/test/java/test/ContactInfoTest.java
+++ addressbook_web_tests/src/test/java/test/ContactInfoTest.java
@@ -54,4 +54,33 @@ public class ContactInfoTest extends TestBase {
         Assertions.assertEquals(expected, ape);
     }
 
+    @Test
+    void testPhoneAddressEmail2() {//Версия с раздеельными проверками 
+        if (app.hbm().getContactCount() == 0){
+            if (app.hbm().getGroupCount() == 0){
+                app.hbm().createGroup(new GroupData("", "groups name", "group header", "group footer"));
+            }app.contacts().createAddress(new ContactData("","First name", "Middle name", "Last name", "Nickname","src/test/resources/images/bzz.jpg", "", "", "", "", "", "", "", ""));
+        }
+        var contacts = app.hbm().getContactList();
+        var contact = contacts.get(0);
+        //var ape = app.contacts().getAddressPhoneEmail(contact);
+        var address = app.contacts().getAddress(contact);
+        var phones = app.contacts().getPhones(contact);
+        var email = app.contacts().getEmail(contact);
+        //var ape = address +"\n"+ phones +"\n"+ email;
+        var expectedAddress = Stream.of(contact.address())
+                .filter(s -> s != null && ! "".equals(s))
+                .collect(Collectors.joining("\n"));
+        var expectedPhones = Stream.of(contact.home(), contact.mobile(), contact.work(), contact.secondary())
+                .filter(s -> s != null && ! "".equals(s))
+                .collect(Collectors.joining("\n"));
+        var expectedEmail = Stream.of(contact.email(), contact.email2(), contact.email3())
+                .filter(s -> s != null && ! "".equals(s))
+                .collect(Collectors.joining("\n"));
+        //Assertions.assertEquals(expected, ape);
+        Assertions.assertEquals(expectedAddress, address);
+        Assertions.assertEquals(expectedPhones, phones);
+        Assertions.assertEquals(expectedEmail, email);
+
+    }
 }
